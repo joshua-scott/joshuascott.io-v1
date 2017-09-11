73922b8bd932b77984b929c8912526e733377d17
Joshua Scott <jscott313@gmail.com>
2017-09-11 11:52:14 +0300

    Added sourcemaps and sass error logging

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', () => {
   return gulp.src('./*.scss')
-    .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./build'))
     .pipe(browserSync.stream());
 });
